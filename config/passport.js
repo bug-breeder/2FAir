6966a148eb409cfc970a3bf589a35cacce41e24d
Anh Nguyen <anhngw@gmail.com>
2024-06-15 15:44:23 +0700

    chore: Update passport.js to comment out Microsoft and Apple strategies

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const MicrosoftStrategy = require('passport-microsoft').Strategy;
-const AppleStrategy = require('passport-apple').Strategy;
+// const MicrosoftStrategy = require('passport-microsoft').Strategy;
+// const AppleStrategy = require('passport-apple').Strategy;
 const User = require('../models/User');
 
 passport.serializeUser((user, done) => {
@@ -33,43 +33,43 @@ passport.use(new GoogleStrategy({
   });
 }));
 
-passport.use(new MicrosoftStrategy({
-  clientID: process.env.MICROSOFT_CLIENT_ID,
-  clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
-  callbackURL: "/auth/microsoft/callback",
-  scope: ['user.read']
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ microsoftId: profile.id }, (err, user) => {
-    if (!user) {
-      const newUser = new User({
-        microsoftId: profile.id,
-        email: profile._json.userPrincipalName,
-        username: profile.displayName
-      });
-      newUser.save().then(user => done(null, user));
-    } else {
-      done(err, user);
-    }
-  });
-}));
+// passport.use(new MicrosoftStrategy({
+//   clientID: process.env.MICROSOFT_CLIENT_ID,
+//   clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
+//   callbackURL: "/auth/microsoft/callback",
+//   scope: ['user.read']
+// }, (accessToken, refreshToken, profile, done) => {
+//   User.findOne({ microsoftId: profile.id }, (err, user) => {
+//     if (!user) {
+//       const newUser = new User({
+//         microsoftId: profile.id,
+//         email: profile._json.userPrincipalName,
+//         username: profile.displayName
+//       });
+//       newUser.save().then(user => done(null, user));
+//     } else {
+//       done(err, user);
+//     }
+//   });
+// }));
 
-passport.use(new AppleStrategy({
-  clientID: process.env.APPLE_CLIENT_ID,
-  teamID: "your_team_id",
-  keyID: "your_key_id",
-  privateKey: fs.readFileSync('path_to_your_key.p8', 'utf8'),
-  callbackURL: "/auth/apple/callback"
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ appleId: profile.id }, (err, user) => {
-    if (!user) {
-      const newUser = new User({
-        appleId: profile.id,
-        email: profile.email,
-        username: profile.email.split('@')[0]
-      });
-      newUser.save().then(user => done(null, user));
-    } else {
-      done(err, user);
-    }
-  });
-}));
+// passport.use(new AppleStrategy({
+//   clientID: process.env.APPLE_CLIENT_ID,
+//   teamID: "your_team_id",
+//   keyID: "your_key_id",
+//   privateKey: fs.readFileSync('path_to_your_key.p8', 'utf8'),
+//   callbackURL: "/auth/apple/callback"
+// }, (accessToken, refreshToken, profile, done) => {
+//   User.findOne({ appleId: profile.id }, (err, user) => {
+//     if (!user) {
+//       const newUser = new User({
+//         appleId: profile.id,
+//         email: profile.email,
+//         username: profile.email.split('@')[0]
+//       });
+//       newUser.save().then(user => done(null, user));
+//     } else {
+//       done(err, user);
+//     }
+//   });
+// }));
--- routes/auth.js
+++ routes/auth.js
@@ -12,17 +12,17 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 });
 
 // Microsoft authentication
-router.get('/microsoft', passport.authenticate('microsoft'));
-router.get('/microsoft/callback', passport.authenticate('microsoft', { failureRedirect: '/' }), (req, res) => {
-  const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
-});
+// router.get('/microsoft', passport.authenticate('microsoft'));
+// router.get('/microsoft/callback', passport.authenticate('microsoft', { failureRedirect: '/' }), (req, res) => {
+//   const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+//   res.json({ token });
+// });
 
 // Apple authentication
-router.get('/apple', passport.authenticate('apple'));
-router.post('/apple/callback', passport.authenticate('apple', { failureRedirect: '/' }), (req, res) => {
-  const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
-});
+// router.get('/apple', passport.authenticate('apple'));
+// router.post('/apple/callback', passport.authenticate('apple', { failureRedirect: '/' }), (req, res) => {
+//   const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+//   res.json({ token });
+// });
 
 module.exports = router;
