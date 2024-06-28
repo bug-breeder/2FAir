74b6acadcbeae5bde96de22a2a9cc80f3b365d1c
Anh Nguyen <anhngw@gmail.com>
2024-06-28 13:34:21 +0700

    chore: Update main layout to fix padding issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-6 px-2 sm:px-6 flex-grow">
+            <main className="container mx-auto max-w-7xl sm:pt-6 px-2 sm:px-6 flex-grow">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
