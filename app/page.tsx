1e2593650209788ee5bc2560dada976e803f4488
Anh Nguyen <anhngw@gmail.com>
2024-06-28 11:16:58 +0700

    refactor: Remove unused things

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
 "use client";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import { useTheme } from "next-themes";
 import OTPCard from "@/components/otp-card";
 
-const generateCode = () => {
-  // Function to generate a new OTP code
-  return Math.floor(100000 + Math.random() * 900000).toString();
-};
-
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-
   const otps = [
     {
       issuer: "Google",
@@ -70,48 +55,6 @@ export default function Home() {
 
   return (
     <section className="flex flex-col items-center justify-center">
-      {/* <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="flat">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div> */}
-
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-2 sm:gap-5">
         {otps.map((otp, index) => (
           <OTPCard key={index} otp={otp} />
