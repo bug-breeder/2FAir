f9b5dd1e0f7233cd90ae0d571f20920fdf0c3758
Anh Nguyen <anhngw@gmail.com>
2024-07-09 00:05:34 +0700

    chore: Remove commented out code in landing page button component

--- landing-page/app/page.tsx
+++ landing-page/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
       <div className="flex gap-3">
         <Link
-          isExternal
+          // isExternal
           className={buttonStyles({
             color: "primary",
             radius: "full",
---
+++ landing-page/components/login.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { Button, Link } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+
+import { FAir } from "@/components/icons";
+
+export default function Login() {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <div className="mt-2 flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 py-6 shadow-small">
+        <div className="flex flex-col items-center pb-2">
+          <FAir size={60} />
+          <p className="text-xl font-medium mt-4">Welcome Back</p>
+          <p className="text-small text-default-500">
+            Log in to your account to continue
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Button
+            startContent={<Icon icon="logos:google-icon" width={24} />}
+            variant="bordered"
+          >
+            Continue with Google
+          </Button>
+          <Button
+            startContent={
+              <Icon
+                className="text-default-500"
+                icon="logos:microsoft-icon"
+                width={24}
+              />
+            }
+            variant="bordered"
+          >
+            Continue with Microsoft
+          </Button>
+        </div>
+        <p className="text-center text-small">
+          Can not access your account?&nbsp;
+          <Link href="#" size="sm">
+            Recovery
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
