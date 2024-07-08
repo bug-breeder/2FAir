5f338d84cfb55547ff9ba7aefe805e87a9897b23
Anh Nguyen <anhngw@gmail.com>
2024-07-08 22:36:01 +0700

    refactor: Update login page component

--- landing-page/app/login/page.tsx
+++ landing-page/app/login/page.tsx
@@ -6,7 +6,7 @@ import { Icon } from "@iconify/react";
 
 import { FAir } from "@/components/icons";
 
-export default function Component() {
+export default function Login() {
   const [isVisible, setIsVisible] = React.useState(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
@@ -21,56 +21,6 @@ export default function Component() {
             Log in to your account to continue
           </p>
         </div>
-        {/* <form
-          className="flex flex-col gap-3"
-          onSubmit={(e) => e.preventDefault()}
-        > */}
-        {/* <Input
-            label="Email Address"
-            name="email"
-            placeholder="Enter your email"
-            type="email"
-            variant="bordered"
-          /> */}
-        {/* <Input
-            endContent={
-              <button type="button" onClick={toggleVisibility}>
-                {isVisible ? (
-                  <Icon
-                    className="pointer-events-none text-2xl text-default-400"
-                    icon="solar:eye-closed-linear"
-                  />
-                ) : (
-                  <Icon
-                    className="pointer-events-none text-2xl text-default-400"
-                    icon="solar:eye-bold"
-                  />
-                )}
-              </button>
-            }
-            label="Password"
-            name="password"
-            placeholder="Enter your password"
-            type={isVisible ? "text" : "password"}
-            variant="bordered"
-          /> */}
-        {/* <div className="flex items-center justify-between px-1 py-2">
-            <Checkbox name="remember" size="sm">
-              Remember me
-            </Checkbox>
-            <Link className="text-default-500" href="#" size="sm">
-              Forgot password?
-            </Link>
-          </div> */}
-        {/* <Button color="primary" type="submit">
-            Log In
-          </Button> */}
-        {/* </form> */}
-        {/* <div className="flex items-center gap-4"> */}
-        {/* <Divider className="flex-1" />
-          <p className="shrink-0 text-tiny text-default-500">OR</p>
-          <Divider className="flex-1" /> */}
-        {/* </div> */}
         <div className="flex flex-col gap-2">
           <Button
             startContent={<Icon icon="logos:google-icon" width={24} />}
@@ -92,7 +42,7 @@ export default function Component() {
           </Button>
         </div>
         <p className="text-center text-small">
-          Can't access your account?&nbsp;
+          Can not access your account?&nbsp;
           <Link href="#" size="sm">
             Recovery
           </Link>
