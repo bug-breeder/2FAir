7f096eea9bbacbdeac0740344788cdbc971956c8
Anh Nguyen <anhngw@gmail.com>
2024-07-14 17:03:37 +0700

    refactor: Remove commented out code for screen height in tailwind.config.js

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       <head />
       <body
         className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
+          "bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
