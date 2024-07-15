417548286ee25163dc538f506d1d50466de4176c
Anh Nguyen <anhngw@gmail.com>
2024-07-15 14:46:02 +0700

    remove useNewUrlParser and useUnifiedTopology since these options are deprecated

--- server/server.js
+++ server/server.js
@@ -27,10 +27,7 @@ app.use("/otp", otpRoutes);
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
